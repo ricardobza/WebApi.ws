af243d186ee730dc8501597f82f6e31da5451e12
Ricardo Baeza <rdobaeza@hotmail.com>
2020-11-25 16:42:43 -0600

    Agregar archivos de proyecto.

---
+++ WebApi.ws/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.ws.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Column(TypeName ="nvarchar(150)")]
+        public string FullName { get; set; }
+    }
+}
